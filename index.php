8069e506845b83a0a922c574e37f270499eff682
Stavros <stavrossavva90@gmail.com>
2021-11-19 15:34:34 +0200

    custom logic to retrive rss feeds and echo into the template. no plugins used. integrated and connected with extra ACF handling

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php get_header();?>
 
 <main>
-
+    <section class="container-fluid" id="welcome">
+        <div class="container text-center">
+            <a href="<?php echo esc_url(site_url('coding-challenge/')); ?>" role="button" class="blue-button">Go to Coding Challenge</a>
+        </div>
+    </section>
 </main>
 
 <?php get_footer();?>
--- page-coding-challenge.php
+++ page-coding-challenge.php
@@ -9,35 +9,126 @@
 
 <main id="code-challenge">
     <section class="container-fluid header-banner">
-        <h1>Trade Now</h1>
-        <p>250+ Forex pairs & FDs on Shares, Indices, Energies & Metals</p>
-        <a href="https://dashboard-eu.bdswiss.com/register" target="_blank" rel="noopener" class="blue-button" role="button">GET STARTED TODAY</a>
+        <?php 
+            if (!class_exists('ACF')) {
+                $videoCode = 'ipUuoMCEbDQ';
+            } else {
+                $videoCode = get_field('video_code');
+            }
+        ?>
+        <iframe src="https://www.youtube.com/embed/<?php echo $videoCode; ?>?playlist=<?php echo $videoCode; ?>&loop=1&autoplay=1&mute=1"></iframe>
+
+        <div class="header-info">
+            <h1>Trade Now</h1>
+            <p>250+ Forex pairs & FDs on Shares, Indices, Energies & Metals</p>
+            <a href="https://dashboard-eu.bdswiss.com/register" target="_blank" rel="noopener" class="blue-button" role="button">GET STARTED TODAY</a>
+        </div>
+
     </section>
 
     <section class="container-fluid rss-section">
         <div class="container">
             <div class="row">
+
+                <?php 
+                    if (!class_exists('ACF')) {
+                        /* Set Default text in case ACF is not installed */
+                        $feed1URL = '';
+                        $feed1Icon = '';
+                        $feed1Description = '';
+                        $feed1Limit = '5';
+
+                        $feed2URL = '';
+                        $feed2Icon = '';
+                        $feed2Description = '';
+                        $feed2Limit = '';
+                    } else {
+                        /* Get user input values */
+                        if(get_field('feed1_url')) : $feed1URL = get_field('feed1_url'); else : $feed1URL = 'https://xml.fxstreet.com/news/forex-news/index.xml'; endif;
+                        if(get_field('feed1_icon')) : $feed1Icon = get_field('feed1_icon'); else : $feed1Icon = get_template_directory_uri() . '/assets/images/fxstreet.png'; endif;
+                        $feed1Description = get_field('feed1_description');
+                        $feed1Limit = get_field('feed1_limit');
+                        
+                        $feed2URL = get_field('feed2_url');
+                        $feed2Icon = get_field('feed2_icon');
+                        $feed2Description = get_field('feed2_description');
+                        $feed2Limit = get_field('feed2_limit');
+                    }
+                ?>
+
                 <div class="col-12 col-md-6">
                     <div class="rss-feed">
-                        <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/fxstreet.png'); ?>" alt="FXSTREET" class="rss-feed--icon">
-                        <p class="rss-feed--desc">FXStreet is a leading source for reliable news and real time Forex analysis. FXStreet offers real-time exchange rates, chart and an economic calendar.</p>
+                        <img src="<?php echo $feed1Icon; ?>" alt="FXSTREET" class="rss-feed--icon">
+                        <p class="rss-feed--desc"><?php echo $feed1Description; ?></p>
                     </div>
 
                     <div class="rss-list">
-                        <?php echo do_shortcode('[rss source="https://www.forexlive.com/feed" posts_limit="5"]'); ?>
+                        <ul class="rss-list">
+                        <?php
+                            $feed = fetch_feed($feed1URL);  
+                            $limit = $feed->get_item_quantity($feed1Limit);
+                            $items = $feed->get_items(0, $limit); 
+
+
+                            if ($limit == 0) echo '<div>The feed is either empty or unavailable.</div>';
+
+                            else foreach ($items as $item) : ?>
+
+                            <li>
+                                <div class="rss-list--icon">
+                                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chart-line" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM464 96H345.94c-21.38 0-32.09 25.85-16.97 40.97l32.4 32.4L288 242.75l-73.37-73.37c-12.5-12.5-32.76-12.5-45.25 0l-68.69 68.69c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L192 237.25l73.37 73.37c12.5 12.5 32.76 12.5 45.25 0l96-96 32.4 32.4c15.12 15.12 40.97 4.41 40.97-16.97V112c.01-8.84-7.15-16-15.99-16z"></path></svg>
+                                </div>
+                                <div class="rss-list-item">
+                                    <span class="rss-list--title">
+                                        <a href="<?php echo $item->get_permalink(); ?>" target="_blank" rel="noopener" title="<?php echo $item->get_title(); ?>"><?php echo $item->get_title(); ?></a>
+                                    </span>
+                                    <span class="rss-list--meta"><?php echo $item->get_date('j F Y @ g:i a'); ?></span>
+                                </div>
+                            </li>
+
+                            <?php endforeach; ?>
+
+                        <ul>
+
+                    </div>
+                </div>
+
+
+                <div class="col-12 col-md-6">
+                    <div class="rss-feed">
+                        <img src="<?php echo $feed2Icon; ?>" alt="Forex Live" class="rss-feed--icon">
+                        <p class="rss-feed--desc"><?php echo $feed2Description; ?></p>
                     </div>
 
-                  <!--   <ul class="rss-list">
-                        <li>
-                            <div class="rss-list--icon">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chart-line" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM464 96H345.94c-21.38 0-32.09 25.85-16.97 40.97l32.4 32.4L288 242.75l-73.37-73.37c-12.5-12.5-32.76-12.5-45.25 0l-68.69 68.69c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L192 237.25l73.37 73.37c12.5 12.5 32.76 12.5 45.25 0l96-96 32.4 32.4c15.12 15.12 40.97 4.41 40.97-16.97V112c.01-8.84-7.15-16-15.99-16z"></path></svg>
-                            </div>
-                            <div class="rss-list-item">
-                                <span class="rss-list--title">EUR/USD Price Analysis: Weekly Doji suggests indecision</span>
-                                <span class="rss-list--meta">Mon Sep 14 2020 0:33:12 UTC</span>
-                            </div>
-                        </li>
-                    </ul> -->
+                    <div class="rss-list">
+                        <ul class="rss-list">
+                        <?php
+                            $feed = fetch_feed($feed2URL);  
+                            $limit = $feed->get_item_quantity($feed2Limit);
+                            $items = $feed->get_items(0, $limit); 
+
+
+                            if ($limit == 0) echo '<div>The feed is either empty or unavailable.</div>';
+
+                            else foreach ($items as $item) : ?>
+
+                            <li>
+                                <div class="rss-list--icon">
+                                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chart-line" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M496 384H64V80c0-8.84-7.16-16-16-16H16C7.16 64 0 71.16 0 80v336c0 17.67 14.33 32 32 32h464c8.84 0 16-7.16 16-16v-32c0-8.84-7.16-16-16-16zM464 96H345.94c-21.38 0-32.09 25.85-16.97 40.97l32.4 32.4L288 242.75l-73.37-73.37c-12.5-12.5-32.76-12.5-45.25 0l-68.69 68.69c-6.25 6.25-6.25 16.38 0 22.63l22.62 22.62c6.25 6.25 16.38 6.25 22.63 0L192 237.25l73.37 73.37c12.5 12.5 32.76 12.5 45.25 0l96-96 32.4 32.4c15.12 15.12 40.97 4.41 40.97-16.97V112c.01-8.84-7.15-16-15.99-16z"></path></svg>
+                                </div>
+                                <div class="rss-list-item">
+                                    <span class="rss-list--title">
+                                        <a href="<?php echo $item->get_permalink(); ?>" target="_blank" rel="noopener" title="<?php echo $item->get_title(); ?>"><?php echo $item->get_title(); ?></a>
+                                    </span>
+                                    <span class="rss-list--meta"><?php echo $item->get_date('j F Y @ g:i a'); ?></span>
+                                </div>
+                            </li>
+
+                            <?php endforeach; ?>
+
+                        <ul>
+
+                    </div>
                 </div>
             </div>
         </div>
